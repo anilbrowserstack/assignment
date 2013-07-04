6f224e2b5d45f409e694390b2ac62a29f644ea57
Anil Kumar Puvathumkadavil <anil@browserstack.com>
2013-07-04 19:44:09 +0530

    multiples of 3 or 5

--- multiples_3_or_5.rb
+++ multiples_3_or_5.rb
@@ -1,13 +1,4 @@
 n = ARGV[0]
-puts "n = #{n}"
 for i in 0..n.to_i do
- if (i % 15 == 0)
-   puts "fizz-buzz"
- elsif (i % 3 ==0)
-   puts "fizz"
- elsif (i %5 == 0) 
-  puts "buzz"
- else
-  puts i
- end 
+ puts (i % 15 == 0) ? "fizz-buzz" : ((i % 3 ==0) ? "fizz" : ((i %5 == 0) ? "buzz" : i))
 end
